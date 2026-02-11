fbae7a8fe271c8e89c778ae3beb028c34cf9f812
DiagnosiZucchero <vlii8rwz@duck.com>
2026-02-11 15:59:57 +0100

    tolti dei print di debug

--- src/qsopt/core/experimental_parameters.py
+++ src/qsopt/core/experimental_parameters.py
@@ -396,11 +396,9 @@ class ExperimentalParameters:
         Otherwise, compute from initial_time, final_time, and time_interval.
         """
         if self.measurement.measurement_times is not None:
-            print("sono nell'IF")
             self._measurement_times_list = [float(t) for t in self.measurement.measurement_times]
             self.measurement.measurement_times = self._measurement_times_list
         else:
-            print("sono nell'Else")
             self._measurement_times_list = self._compute_measurement_times_from_interval()
             self.measurement.measurement_times = self._measurement_times_list
 
